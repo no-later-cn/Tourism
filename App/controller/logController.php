f0cda451aad41cfc0b95a5541acdebe7db1c7c88
baixiaojun <1217474722@qq.com>
2017-04-10 12:39:46 +0800

    baixiaojuan

---
+++ App/controller/logController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/10
+ * Time: 11:20
+ */
+namespace App\Controller;
+
+use Core\Framework;
+
+class logController extends Framework{
+    //网站首页
+    public function index(){
+        $this->display('index/index.html');
+    }
+    //旅行日志
+    public function Travellog(){
+        $this->display('log/Travellog.html');
+    }
+    //日志详情
+    public  function  Logdetails(){
+        $this->display('log/Logdetails.html');
+    }
+    //日志发布
+    public function publish(){
+        $this->display('log/Development.html');
+    }
+}

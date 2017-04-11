94c9864ba8977ee73888d9f172a624f221b72fe7
我心飞扬sxj385 <1045020949@qq.com>
2017-04-12 03:10:08 +0800

    php

--- App/controller/indexcontroller.php
+++ App/controller/indexcontroller.php
@@ -7,13 +7,24 @@ class indexController extends Framework{
         $this->display("index/index.html");
     }
     public function heatPlay(){
+        $this->assign('header_t',"当季热玩");
+        $this->assign("icon_l",true);
+        $this->assign('url','/index');
+        $this->assign("icon_r",true);
+        $this->assign('header_d',"HOT PLAY");
         $this->display("index/heatPlay.html");
     }
     public function heatPlay_details(){
+        $this->assign('header_t',"景点详情");
+        $this->assign("url","/index/".$_GET['sign']);
+        $this->assign("icon_l",true);
+        $this->assign("icon_r",true);
+        $this->assign('header_d',"ATTRACTIONS DETAILS");
         $this->display("index/heatPlay_details.html");
     }
     public function heatSpots(){
         $this->assign("method","index");
+        $this->assign('url','/index');
         $this->assign("header_t","热门景点");
         $this->assign("icon_l",true);
         $this->assign("header_d","HOT SPOTS");
@@ -24,6 +35,11 @@ class indexController extends Framework{
         $this->display("index/logdetails.html");
     }
     public function travel(){
+        $this->assign('header_t',"旅行攻略");
+        $this->assign('url','/index');
+        $this->assign("icon_l",true);
+        $this->assign("icon_r",true);
+        $this->assign('header_d',"TRAVEL STRATEGY");
         $this->display("index/travel.html");
     }
 

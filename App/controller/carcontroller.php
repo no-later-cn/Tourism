ea236370abae57e3e4b515e308daa9a5ab86d792
feikool <feikool@sina.com>
2017-04-18 16:51:09 +0800

    test

--- App/controller/carcontroller.php
+++ App/controller/carcontroller.php
@@ -1,17 +1,25 @@
 <?php
 namespace App\Controller;
 use Core\Framework;
-
+use App\model\carmodel;
 class carController extends Framework {
+	public $car;
+	function __construct(){
+		require MODEL.'carmodel.php';
+		$this->car = new carmodel();
+	}
 	public function index() {
 		$this->assign("method", "car");
 		$this->assign("header_t", "租车服务");
 		$this->assign("header_d", "CAR RENTAL");
 		$this->assign("icon_r", true);
+		$this->assign("data",$this->car->getCar());
 		$this->display('car/cart.html');
 	}
 	public function detailed() {
+		$id = $_GET['carid'];
 		$this->assign("header_t", "租车详情");
+		$this->assign("data",$this->car->getdetails($id));
 		$this->assign("header_d", "CAR DETAILED");
 		$this->assign("icon_l", true);
 		$this->assign("icon_r", true);
--- App/model/paymodel.php
+++ App/model/paymodel.php
@@ -1,15 +1,11 @@
-<?php
+<?php 
 namespace App\model;
 use Core\DBPDO;
 
-class carmodel extends DBPDO {
-	function getCar(){
-		return $this->fetchAll('select * from car');
-	}
-	function getdetails($id){
+class paymodel extends DBPDO{
+	function getshoping($id){
 		$con = $this->PDO->prepare('select * from car where id='.$id);
         $con->execute();
         return $con->fetchAll();
-
 	}
 }
